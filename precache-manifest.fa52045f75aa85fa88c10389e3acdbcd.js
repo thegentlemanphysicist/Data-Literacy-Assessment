ac496f48f39e0a12743a8f19effe0cbfcf69b431
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-05-14 15:57:28 -0400

    Updates

---
+++ precache-manifest.fa52045f75aa85fa88c10389e3acdbcd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13e6d81d73f6019513e96b7a94294aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80b97f270f4b49dc8d7",
+    "url": "/static/css/2.3082a726.chunk.css"
+  },
+  {
+    "revision": "f80b6cebcfa55871db30",
+    "url": "/static/css/main.d6e1241f.chunk.css"
+  },
+  {
+    "revision": "b80b97f270f4b49dc8d7",
+    "url": "/static/js/2.9bb96c2b.chunk.js"
+  },
+  {
+    "revision": "8cc9c34a96de9b405f96a5fa102e6f69",
+    "url": "/static/js/2.9bb96c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80b6cebcfa55871db30",
+    "url": "/static/js/main.70bb8f7f.chunk.js"
+  },
+  {
+    "revision": "5c67e529e4d136a91d5e",
+    "url": "/static/js/runtime-main.f10a1050.js"
+  },
+  {
+    "revision": "e3b6896cf0e58d7fd4abc266f496fee5",
+    "url": "/static/media/Introduction.e3b6896c.md"
+  },
+  {
+    "revision": "f1e301379b6faf6fdc7161a77b623da9",
+    "url": "/static/media/Introduction.f1e30137.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/data-literacy-assessment/precache-manifest.36f1b2f1215eb2053c32746f9caa2a04.js"
+  "/precache-manifest.fa52045f75aa85fa88c10389e3acdbcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/data-literacy-assessment/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
